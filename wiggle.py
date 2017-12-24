bacc6d164287d3db31f02aaf94cb5586d1cb8cb2
Unknown <brandon.aleson@gmail.com>
2017-12-24 13:22:49 -0800

    Wiggler class

--- wiggle.py
+++ wiggle.py
@@ -1,46 +1,57 @@
 #!/usr/bin/python3
 # gpiozero - control DC motor
 # 12/15/17
-# updated: 12/15/17
+# updated: 12/24/17
 
 import time
 import numpy
-from gpiozero import Motor
+import gpiozero
 
 
-sleep_time = 0.05
-interval = 21
+class Wiggler(gpiozero.Motor):
+    '''
+    class to extend gpiozero.Motor, which has following methods:
+    forward(speed=float)
+    backward(speed=float)
+    stop()
 
-# arange below results in: array([ 0. ,  0.1,  0.2,  0.3,  0.4,  0.5,  0.6,  0.7,  0.8,  0.9])
-# steps = numpy.arange(0.0, 1.0, 0.1)
-steps = numpy.linspace(0, 1, interval)
+    speed can only be 0 or 1 if pwm!=True when constructing gpiozero.Motor
+    more here: https://gpiozero.readthedocs.io/en/stable/api_output.html#motor
+    '''
 
+    def __init__(self, pins, pwm=True, sleep_time=0.05, interval=21):
+        '''
+        numpy.arange results in: array([0. ,  0.1,  0.2,  0.3,  0.4,  0.5,  0.6,  0.7,  0.8,  0.9])
+        using numpy.linspace to create steps ensures the last number will always be 1
+        '''
 
-def ramp_up(motor, steps):
-    for step in steps:
-        motor.forward(speed=step)
-        time.sleep(sleep_time)
+        gpiozero.Motor.__init__(self, *pins, pwm=pwm, *args, **kwargs)
+        self.sleep_time = sleep_time
+        self.interval = interval
+        self.steps = numpy.linspace(0, 1, self.interval)
+        self.current_speed = 0
 
-    return step
+    def ramp_up(self):
+        for step in self.steps:
+            self.forward(speed=step)
+            self.current_speed = step
+            time.sleep(self.sleep_time)
 
+    def ramp_down(self):
+        '''have to add 1 to because regular indexing starts @ 0, but reverse indexing starts @ -1'''
 
-def ramp_down(motor, steps):
-    '''have to add 1 to because regular indexing starts @ 0, but reverse indexing starts @ -1'''
-
-    for step in range(len(steps)):
-        step += 1
-        step *= -1
-        motor.forward(speed=steps[step])
-        time.sleep(sleep_time)
-
-    return step
-
+        for step in range(len(self.steps)):
+            step += 1
+            step *= -1
+            self.forward(speed=steps[step])
+            self.current_speed = step
+            time.sleep(self.sleep_time)
 
 
 if __name__ == '__main__':
     pins = (5, 6)
-    motor = Motor(*pins)
+    wiggler = Wiggler(*pins)
 
-    current_speed = ramp_up(motor, steps)
+    wiggler.ramp_up()
     time.sleep(2)
-    ramp_down(motor, steps)
+    wiggler.ramp_down()
